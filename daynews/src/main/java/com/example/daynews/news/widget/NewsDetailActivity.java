712c299f1b3593a763aed7c869dcace16ba082ab
winchances <409736781@qq.com>
2016-06-04 09:15:41 +0800

    添加卡片式布局

--- daynews/src/main/java/com/example/daynews/news/widget/NewsDetailActivity.java
+++ daynews/src/main/java/com/example/daynews/news/widget/NewsDetailActivity.java
@@ -1,11 +1,18 @@
 package com.example.daynews.news.widget;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by wenc on 2016/6/3.
  */
 public class NewsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
 
 }
